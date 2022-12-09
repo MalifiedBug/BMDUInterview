45702a03a2e0a988bb82eccaf8b1e0c1657962ab
omkar lolge <omkarlolge119.ol@gmail.com>
2022-12-09 15:21:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navibar from './Navibar';
 import Carousel2 from './Carousel2';
 import Text1 from './Text1';
 import Blocks from './Blocks';
-import { NavItem } from 'react-bootstrap';
 
 function App() {
   return (
